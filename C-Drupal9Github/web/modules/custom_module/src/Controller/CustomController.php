079db4067593933c71268209f8bc82af44366c1c
Someshver Thakur <someshverthakur@gmail.com>
2023-04-22 12:35:33 +0530

    Custom Route Using custom module

---
+++ C-Drupal9Github/web/modules/custom_module/src/Controller/CustomController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\custom_module\Controller;
+
+class CustomController
+{
+
+  public function getContent(){
+
+    return [
+      "#markup" => "This is content"
+    ];
+  }
+}
